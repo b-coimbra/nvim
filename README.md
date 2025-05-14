61c3dfd7bd22f801b0b68a42dc0e28c8637cd573
b-coimbra <b-coimbra@hotmail.com>
2025-05-14 13:07:02 -0300

    feat: customize dashboard

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # My Neovim Configuration
 
+<p align="center">
+  <img src="https://i.imgur.com/N32Nnfu.png">
+</p>
+
 ## Dependencies
 
+- `nvim v0.12`
 - `ripgrep` for Telescope fuzzy finding
 
 ## Installation
 
 1. Clone into **~/.config/nvim**: `$ git clone https://github.com/b-coimbra/nvim.git ~/.config/nvim`
-2. Install a [Nerd font](https://www.nerdfonts.com/font-downloads) for icon support
-3. Install [Yazi](https://yazi-rs.github.io/docs/installation) for a built-in file manager
+2. Install one of the [Nerd fonts](https://www.nerdfonts.com/font-downloads) for icon support
+3. Install [Yazi](https://yazi-rs.github.io/docs/installation) for a built-in file manager (bound to `<leader>-`)
 4. Install preferred language servers with [:Mason](https://github.com/mason-org/mason.nvim)
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,5 +1,43 @@
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  config = function()
+    require("dashboard").setup({
+      theme = "hyper",
+      config = {
+        header = {
+          "┌─────┐",
+          "│ •ᴗ• │",
+          "│     │",
+          "└─────┘",
+        },
+        shortcut = {
+          {
+            icon = "󰆧 ",
+            desc = "home",
+            action = "vim.ui.open('https://github.com/b-coimbra/nvim')",
+            key = "h",
+          },
+          {
+            icon = "󰁯 ",
+            group = "Added",
+            desc = "update",
+            action = "Lazy update",
+            key = "u",
+          },
+          {
+            icon = " ",
+            group = "@label",
+            desc = "config",
+            action = function()
+              require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+            end,
+            key = "c",
+          },
+        },
+        footer = {},
+      },
+    })
+  end,
 }
